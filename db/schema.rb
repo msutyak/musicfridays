5f793e29ec63b36984deb23acdc4a90e7c09fe99
msutyak <msutyak@gmail.com>
2014-12-11 17:01:24 -0800

    through pagination tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20141114000137) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
     t.string   "remember_digest"
   end
 
